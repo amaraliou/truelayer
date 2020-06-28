279230f7b95bc610b7e1fc386f70c7f4ed432329
Aliou Amar <amarraliou97@gmail.com>
2020-06-28 19:47:44 +0100

    GetCards

---
+++ card.go
@@ -0,0 +1,35 @@
+package truelayer
+
+import "time"
+
+// Card ..
+type Card struct {
+	AccountID     string    `json:"account_id"`
+	Network       string    `json:"card_network"`
+	Type          string    `json:"card_type"`
+	Currency      string    `json:"currency"`
+	DisplayName   string    `json:"display_name"`
+	PartialNumber string    `json:"partial_card_number"`
+	NameOnCard    string    `json:"name_on_card"`
+	ValidFrom     string    `json:"valid_from"`
+	ValidTo       string    `json:"valid_to"`
+	UpdatedAt     time.Time `json:"update_timestamp"`
+	Provider      *Provider `json:"provider"`
+}
+
+// GetCards ...
+func (client *Client) GetCards() ([]*Card, error) {
+
+	truelayerURL := client.baseURL + "data/v1/cards"
+
+	var cards struct {
+		Results []*Card `json:"results"`
+	}
+
+	err := client.get(truelayerURL, &cards)
+	if err != nil {
+		return nil, err
+	}
+
+	return cards.Results, nil
+}
